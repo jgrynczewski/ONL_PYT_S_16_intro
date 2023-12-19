e979d2e78a4529046a624ba098376c8e03d6c1e7
jgrynczewski <jgrynczewski@gmail.com>
2023-12-19 12:53:58 +0100

    zadania part2

--- project/exercises_app/models.py
+++ project/exercises_app/models.py
@@ -3,5 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Band(models.Model):
+    CHOICES = [
+        (-1, 'not defined'),
+        (0, 'rock'),
+        (1, 'metal'),
+        (2, 'pop'),
+        (3, 'hip - hop'),
+        (4, 'electronic'),
+        (5, 'reggae'),
+        (6, 'other')
+    ]
+
     name = models.CharField(max_length=64)
     year = models.IntegerField(null=True)
+    still_active = models.BooleanField(default=True)
+    genre = models.IntegerField(choices=CHOICES, default=-1)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
