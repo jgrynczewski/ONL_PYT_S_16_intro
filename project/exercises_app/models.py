5f3553b919b604e2c65754b30e849da70390fea3
jgrynczewski <jgrynczewski@gmail.com>
2023-12-19 14:14:40 +0100

    zadania modele

--- project/exercises_app/models.py
+++ project/exercises_app/models.py
@@ -23,3 +23,34 @@ class Band(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(null=True)
+
+
+class Article(models.Model):
+    STATUS_CHOICES = [
+        (0, 'in progress'),
+        (1, 'wait for acceptance'),
+        (2, 'published')
+    ]
+
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=64, null=True)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    emission_start = models.DateField(null=True)
+    emission_end = models.DateField(null=True)
+
+
+class Album(models.Model):
+    RATING_CHOICES = [
+        (0, '0 stars'),
+        (1, '1 star'),
+        (2, '2 stars'),
+        (3, '3 stars'),
+        (4, '4 stars'),
+        (5, '5 stars'),
+    ]
+
+    title = models.CharField(max_length=128)
+    year = models.IntegerField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
