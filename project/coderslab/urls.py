9ec869449c5832153a0c4e22c86fc2e553fc1090
jgrynczewski <jgrynczewski@gmail.com>
2023-12-20 10:06:35 +0100

    zadania modele 2

--- project/coderslab/urls.py
+++ project/coderslab/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from exercises_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', views.article_view),
 ]
--- project/exercises_app/views.py
+++ project/exercises_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from exercises_app.models import Article
 
 # Create your views here.
+def article_view(request):
+
+    articles = Article.objects.filter(status=2)
+
+    response = '<ul>'
+    for article in articles:
+        if not article.author is None:
+            response += f"<li>{article.title} {article.author} {article.date_added}</li>"
+        else:
+            response += f"<li>{article.title} {article.date_added}</li>"
+
+    response += '</ul>'
+
+    return HttpResponse(response)
